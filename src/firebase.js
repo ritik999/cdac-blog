372f1db0ebccc9a9e5ad09c8eb234c722e72263c
ritik999 <uritik85@gmail.com>
2024-02-21 23:59:07 +0530

    added .env for ref

--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// VITE_FIREBASE_API_KEY="AIzaSyCEVq2sUR0hfz28TeJAHL1KAvc9oj7g8mk"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "cdac-blo.firebaseapp.com",
